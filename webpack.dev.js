5b39a36f5d9a3b915a9dc464a73ca99bfa9c714a
Pablo Grillo <pablo.grillo@roiback.com>
2020-12-21 11:55:28 +0100

    fix(webpack-conf): change webpack dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,59 +1,33 @@
 /* eslint "import/no-extraneous-dependencies": ["error", {"devDependencies": true }] */
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        library: 'Example',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-react',
-                            '@babel/preset-env',
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'template/index.html',
-        }),
-        new MiniCssExtractPlugin(),
-    ],
+const ENTRY_POINTS = {
+    index: ['./src/index.js'],
+};
+
+const OUTPUT_CONFIG = {
+    // An absolute path to the desired output directory.
+    path: path.resolve(__dirname, 'dist/'),
+
+    // A filename pattern for the output files
+    filename: 'index.js',
+
+    // IMPORTANT!: This is the name of the global variable exported in browsers
+    // Change it for the name you want your component to have as window.NAME
+    library: 'PlayScss',
+
+    libraryTarget: 'umd',
+};
+
+module.exports = merge(common, {
+    mode: 'development',
+    entry: ENTRY_POINTS,
+    output: OUTPUT_CONFIG,
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         open: true,
         port: 8000,
     },
-};
+});
